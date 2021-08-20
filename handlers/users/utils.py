506cc0e064aeb3ff2ba4f2ffc43beb5aabcc75e3
Chenger1 <exs2199@gmail.com>
2021-08-20 22:47:19 +0300

    Fix whitespaces

--- handlers/users/utils.py
+++ handlers/users/utils.py
@@ -6,7 +6,7 @@ from keyboards.default.defaults import geo_keyboard
 
 async def get_user_age(data: dict) -> str:
     if data.get('age'):
-        text = f'Текущий возраст {data["age"]}.\n Введите новый'
+        text = f'Текущий возраст {data["age"]}.\nВведите новый'
     else:
         text = 'Введите ваш возраст'
     return text
@@ -15,7 +15,7 @@ async def get_user_age(data: dict) -> str:
 async def get_user_gender(data: dict, key) -> str:
     if data.get(key):
         gender = await User.get_gender_display(data[key])
-        text = f'Текущий пол: <b>{gender}</b>.\n Выберите новый'
+        text = f'Текущий пол: <b>{gender}</b>.\nВыберите новый'
     else:
         if key == 'gender':
             text = 'Выберите пол'
@@ -45,7 +45,7 @@ async def get_user_location(data: dict, m: Message):
     if data.get('longitude') and data.get('latitude'):
         longitude = data['longitude']
         latitude = data['latitude']
-        message = await m.answer('Текущее местоположение.\n Выберите новое',
+        message = await m.answer('Текущее местоположение.\nВыберите новое',
                                  reply_markup=geo_keyboard)
         await m.bot.send_location(chat_id=m.from_user.id,
                                   longitude=longitude, latitude=latitude,
